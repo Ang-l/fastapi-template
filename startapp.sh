64ec23ae09990ebe0c409482fe3b6cc8e386b6f3
Ang-l <lijiang_lja@163.com>
2024-03-10 06:33:30 +0800

    update-Adjusting Script Format

--- startapp.sh
+++ startapp.sh
@@ -1,51 +1,47 @@
-# Author: Ang-l
-# github: https://github.com/Ang-l/fastapi-template
-# Date: 2024-03-10
-# email: lijiang_lja@163.com
-
 #!/bin/bash
 
-appname=$1
+# Author: Ang-l
+# GitHub: https://github.com/Ang-l/fastapi-template
+# Date: 2024-03-10
+# Email: lijiang_lja@163.com
 
-if [ -z "$appname" ]; then
-    echo "app名称不存在将终止进程...."
-    exit
-fi
+APP_DIR="application"
+BASE_DIR="$(dirname "$0")"
+DATABASE_FILE="$BASE_DIR/db/base.py"
+MAIN_FILE="$BASE_DIR/main.py"
 
-DIRECTORY="$(dirname "$0")/application/$appname/"
+create_app() {
+    local appname=$1
 
-if [ -d "$DIRECTORY" ]; then
-  echo "app $appname 已存在即将终止进程...."
-  exit
-fi
+    if [ -z "$appname" ]; then
+        echo "未提供应用名称，请终止进程..."
+        exit 1
+    fi
 
+    local DIRECTORY="$BASE_DIR/$APP_DIR/$appname"
+    if [ -d "$DIRECTORY" ]; then
+        echo "应用 $appname 已存在，请终止进程..."
+        exit 1
+    fi
 
-create_app(){
-    mkdir_app_dir
-    touch_py
-    add_mysql_info
-    add_router
-}
+    mkdir -p "$DIRECTORY"
+    touch "$DIRECTORY/__init__.py" "$DIRECTORY/model.py" "$DIRECTORY/views.py" "$DIRECTORY/schemas.py"
 
-mkdir_app_dir() {
-    mkdir -p "$(dirname "$0")/application/$appname/"
-}
+    add_mysql_info "$appname"
 
-touch_py() {
-    touch $DIRECTORY/__init__.py $DIRECTORY/model.py $DIRECTORY/views.py $DIRECTORY/schemas.py 
+    add_router "$appname"
 }
 
 add_mysql_info() {
-
-    cat << EOF >> db/base.py
-
+    local appname=$1
+    cat << EOF >> "$DATABASE_FILE"
 from application.$appname import model
 EOF
-
 }
 
 add_router() {
-    cat << EOF >> main.py
+    local appname=$1
+    cat << EOF >> "$MAIN_FILE"
 
 from application.$appname import views as view_$appname
 
@@ -53,20 +49,19 @@ app.include_router(
     view_$appname.router,
     prefix="/v1/$appname",
     responses={404: {"description": "访问的页面不存在"}, 405:{"description": "请求方式异常"}},
-
 )
-
 EOF
 
-   cat << EOF >> application/$appname/views.py
+    cat << EOF >> "$BASE_DIR/$APP_DIR/$appname/views.py"
 from fastapi import APIRouter
-
-
 
 router = APIRouter()
 EOF
-
-
 }
 
-create_app
+main() {
+    local appname=$1
+    create_app "$appname"
+}
+
+main "$@"
