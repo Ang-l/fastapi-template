4e25700807e84ed3a9111a2485c80107e2086c73
Ang-l <lijiang_lja@163.com>
2024-03-11 05:32:03 +0800

    fix:Resolve duplicate introductions and excess files

--- db/__init__.py
+++ db/__init__.py
@@ -2,5 +2,3 @@
 # github: https://github.com/Ang-l/fastapi-template
 # Date: 2024-03-10
 # email: lijiang_lja@163.com
-
-from .base_class import Base
--- main.py
+++ main.py
@@ -5,14 +5,5 @@
 
 from fastapi import FastAPI
 
-from application.users import views as user_view
-
-
 app = FastAPI()
 
-app.include_router(
-    user_view.router,
-    prefix="/v1/users",
-    responses={404: {"description": "访问的页面不存在"}, 405:{"description": "请求方式异常"}},
-
-)
