749138c45f562a8df2f6ceb39c4b13ff2b7df472
Ang-l <lijiang_lja@163.com>
2024-03-14 20:20:48 +0800

    feat:主入口新增中间件

--- main.py
+++ main.py
@@ -3,7 +3,24 @@
 # Date: 2024-03-10
 # email: lijiang_lja@163.com
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def custom_middleware(request: Request, call_next):
+    response = await call_next(request)
+    
+    return response
 
