e0ab6828755788ee96681192ea2051195f38c949
Ang-l <68832918+Ang-l@users.noreply.github.com>
2024-06-15 15:51:46 +0800

    feat: 配置文件中加入redis信息

--- settings/dev.py
+++ settings/dev.py
@@ -4,5 +4,19 @@
 # email: lijiang_lja@163.com
 
 
-# 数据库相关配置
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345678@localhost:3306/backend_fix?charset=utf8mb4"
+# 数据库相关配置、使用mysql
+DATABASE_HOST = ""
+DATABASE_PORT = ""
+DATABASE_USER = ""
+DATABASE_PASSWORD = ""
+
+DATABASE_NAME = ""
+DATABASE_LANGUAGE = "utf8mb4"  # 固定参数一般不需要调整
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}?charset={DATABASE_LANGUAGE}"
+
+
+### redis
+REDIS_HOST = ""
+REDIS_PORT = ""
+REDIS_PASSWORD = ""
