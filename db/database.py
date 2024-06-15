5e7e5cdf403a1c6848fb7a88420a481640455887
Ang-l <68832918+Ang-l@users.noreply.github.com>
2024-06-15 15:53:23 +0800

    feat: 加入redis相关链接函数

--- db/database.py
+++ db/database.py
@@ -1,18 +1,31 @@
+
+import aioredis
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from settings.settings import SQLALCHEMY_DATABASE_URL
+from settings.settings import SQLALCHEMY_DATABASE_URL, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-def get_db():
+def get_db():    # 数据库、mysql
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
- 
+
+
+async def get_redis():    # redis
+
+    redis_url = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}"
+
+    redis = aioredis.from_url(redis_url, encoding="utf-8", decode_responses=True)
+
+    try:
+        yield redis
+    finally:
+        await redis.close()
