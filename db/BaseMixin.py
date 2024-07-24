949b9e83d1a20bbb221db0bcf7a959e6fd095758
Ang-l <68832918+Ang-l@users.noreply.github.com>
2024-07-24 18:06:48 +0800

    feat: 描述base created_at 时间描述 utc时间

--- db/BaseMixin.py
+++ db/BaseMixin.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 class BaseMixin:
     id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)    ##### 时间使用的是utc的时间、可以根据业务调整
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     is_deleted = Column(Boolean, default=False, comment="状态")
 
